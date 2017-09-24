cdeed719c3fa507ba219c502b366b28b26ec6567
Khaled Shaaban <kshaaban@outlook.com>
2017-09-24 00:01:02 -0400

    create badge component

--- stencil.config.js
+++ stencil.config.js
@@ -1,6 +1,7 @@
 exports.config = {
   bundles: [
     { components: ['stencil-bootstrap-demo', 'alerts-page', 'scb-alert'] },
+    { components: ['scb-badge'] },
   ],
   collections: [
     { name: '@stencil/router' }
